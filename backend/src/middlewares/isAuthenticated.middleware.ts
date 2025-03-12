9f3f19fd7de94048f122ba0c8471159341d924aa
Abdul Ahad <abdul9ahad@gmail.com>
2025-03-12 15:41:00 +0500

    feat: add member model and role-based middleware

---
+++ backend/src/middlewares/isAuthenticated.middleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from 'express';
+import { UnauthorizedException } from '../utils/appError';
+
+const isAuthenticated = async (req: Request, _res: Response, next: NextFunction) => {
+  try {
+    if (!req.user || !req.user._id) {
+      throw new UnauthorizedException('Unauthorized. Please log in.');
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default isAuthenticated;
---
+++ backend/src/utils/roleGuard.ts
@@ -0,0 +1,16 @@
+import { PermissionType } from '../enums/role.enum';
+import { UnauthorizedException } from './appError';
+import { RolePermissions } from './role-permission';
+
+export const roleGuard = (
+  role: keyof typeof RolePermissions,
+  requiredPermission: PermissionType[]
+) => {
+  const permission = RolePermissions[role];
+  const hasPermission = requiredPermission.every((p) => permission.includes(p));
+  if (!hasPermission) {
+    throw new UnauthorizedException(
+      'You do not have the necessary permissions to perform this action'
+    );
+  }
+};
