7a53c5482f0a59af32b1d2163b53205b0bab1705
Abdul Ahad <abdul9ahad@gmail.com>
2025-03-25 14:16:00 +0500

    feat: add task table component with basic columns

---
+++ frontend/src/components/workspace/task/edit-task-dialog.tsx
@@ -0,0 +1,27 @@
+import { Dialog, DialogContent } from '@/components/ui/dialog';
+import EditTaskForm from './edit-task-form';
+
+const EditTaskDialog = (props: {
+  projectId: string;
+  taskId: string;
+  opened?: boolean;
+  fromAllTask: boolean;
+  onClose: () => void;
+}) => {
+  return (
+    <Dialog open={props?.opened} onOpenChange={props?.onClose}>
+      <DialogContent className="sm:max-w-lg max-h-auto my-5 border-0">
+        <EditTaskForm
+          key={props.taskId.toString()}
+          projectId={props.projectId}
+          onClose={props.onClose}
+          taskId={props.taskId}
+          fromAllTask={props.fromAllTask}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default EditTaskDialog;
+
