4746622e488b4dfd6cec8b28055c76898ad28655
Abdul Ahad <abdul9ahad@gmail.com>
2026-04-02 21:45:09 +0200

    fix: session store in local storage issue

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -26,8 +26,7 @@ export const useStoreBase = create<StoreType>()(
         ...createAuthSlice(...a),
       })),
       {
-        name: 'session-storage', // Name of the item in localStorage (or sessionStorage)
-        getStorage: () => sessionStorage, // (optional) by default it's localStorage
+        name: 'session-storage',
       }
     )
   )
